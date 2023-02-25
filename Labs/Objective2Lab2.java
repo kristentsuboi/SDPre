deeee4c5bf8a9a36d14dbd7ad5d287bc82adf22e
kristentsuboi <kristenatsuboi@gmail.com>
2023-02-25 10:39:03 -0600

    complete objective2lab2

--- Labs/Objective2Lab2.java
+++ Labs/Objective2Lab2.java
@@ -1,8 +1,8 @@
 public class Objective2Lab2 {
   public static void main(String[] args) {
-    String name = "Charles Peterson";
-    int age = 47;
-    String state = "Arkansas";
+    String name = "Kristen Tsuboi";
+    int age = 30;
+    String state = "Texas";
 
     System.out.println("Name: " + name + " Age: " + age + " State: " + state);  }
 }
