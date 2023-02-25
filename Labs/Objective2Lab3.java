d3a322b6468970dc7dc60bac0f3f058d3b11987e
kristentsuboi <kristenatsuboi@gmail.com>
2023-02-25 13:01:59 -0600

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+
+    int year = 1992;
+    String month = "June";
+    boolean weekend = false;
+    System.out.println(year);
+    System.out.println(month);
+    System.out.println(weekend);
+  }
+}
